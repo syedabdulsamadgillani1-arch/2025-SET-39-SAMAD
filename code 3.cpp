ec13e23cdc077bd3d2cca9b327fc201143f4dbdb
Syed Abdul Samad <syedabdulsamadgillani1@gmail.com>
2025-11-26 21:35:35 +0500

    Add files via upload

---
+++ code 3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+cout<<"+------------+\n";
+cout<<"\tRoll No\t\tMarks\t|\n";
+cout<<"+-------------+\n";
+cout<<"|\t101\t|\t89\t|\n";
+cout<<"\t102\t|\t92\t|\n"; 
+cout<<"\t103\t|\t85\t|\n";
+cout<<"+-----------+\n";
+return 0;
+}
