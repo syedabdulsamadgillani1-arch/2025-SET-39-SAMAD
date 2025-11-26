44461bbf645356f3aa1c3138dcbc3f3faa77afa0
Syed Abdul Samad <syedabdulsamadgillani1@gmail.com>
2025-11-26 21:28:05 +0500

    Add files via upload

---
+++ code 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"------------\n";
+cout<<"\tStudent ID Card\t\n";
+cout<<"------------\n;
+cout<<"Name:\t\tAbdul Samad\n";
+cout<<"Roll No:\t12345\n";
+cout<<"Department:\tSoftware Engeneering\n";
+cout<<"Session:\t2024-2028\n";
+cout<<"----------\n;
+return 0;
+}
