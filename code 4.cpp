018aa5339490f3caeb1810688551c374d9c73eee
Syed Abdul Samad <syedabdulsamadgillani1@gmail.com>
2025-11-26 21:36:12 +0500

    Add files via upload

---
+++ code 4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main
+{
+cout<<"******MENU******\n";
+cout<<"1. Biryani\tRs. 350\n";
+cout<<"2. Karahi\tRs. 600\n";
+cout<<"3. Qorma\tRs. 500\n";
+cout<<"4. Drinks\tRs. 100\n";
+cout<<"********************\n";
+return 0;
+}
