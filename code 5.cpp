c3fd9f0f361707dad7803709ebfda8e19981ef55
Syed Abdul Samad <syedabdulsamadgillani1@gmail.com>
2025-11-26 21:37:55 +0500

    Add files via upload

---
+++ code 5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   string Name, Department, Address;
+   int Roll No;
+cout<<"------------------"<<endl;
+cout<<"Name: ";
+cin>>Name;
+cout<<"Roll No: ";
+cin>>Roll_No;
+cout<<"Department: ";
+cin>>Department;
+cout<<"Address: ";
+cin>>Address;
+return 0;
+}
